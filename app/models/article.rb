5f5997e9d6042cdbdab2ac49cd96558d721e9e07
lanlh <lanlh@atmarkcafe.org>
2019-05-24 12:36:25 +0700

    Fix validate article

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   
   validates :title, presence: true, length: { minimum: 3, maximum: 300 }
   validates :short_description, presence: true, length: { minimum: 10, maximum: 1000 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 8000 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 30000 }
 
   scope :created_at_desc, -> { order(created_at: :desc) }
   mount_uploader :image, ImageUploader
